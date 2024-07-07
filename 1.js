002c5319e19dfa5f70d4bfbea3f0b79897f62adf
MadhuBasava4832 <madhubasava32@gmail.com>
2024-07-07 20:08:50 +0530

    basics

---
+++ 1.js
@@ -0,0 +1,9 @@
+// console.log(x)
+// var d=[1,2,3,4,5,6,7,8,9]
+// d.push(10)
+// d.splice(2,0,2.1,2.2,2.3)
+// console.log(d)
+// console.log(d.slice(2,7))
+// d.map(name =>{
+//     console.log(name)
+// })
---
+++ task_1/1.js
@@ -0,0 +1,12 @@
+var table = document.createElement("table")
+table.style.border="1px"
+table.className="ta"
+
+
+
+
+
+
+function fun(){
+   console.log(table)
+}
